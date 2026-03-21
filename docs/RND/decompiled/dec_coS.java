92ad2daaf4fefaa8d2f81d5ec668ad41482101ee
WarframeBuilder <warframe.builder@example.com>
2026-03-21 19:19:02 +0100

    Add decompiled classes and related changes
    - Introduced new decompiled Java classes including `coS`, `coR`, `cqv`, `cqw`, `crA`, `crB`, `crs`, `crt`, `crv`, `crw`, `crx`, `cry`, `crz`, `csn`, `cso`, `csp`, and `csq`.
    - Each class includes methods for handling byte arrays and buffer operations.
    - Updated the structure of the classes to reflect the decompiled format, ensuring compatibility with existing code.
    - Added necessary imports and method signatures to maintain functionality.

---
+++ docs/RND/decompiled/dec_coS.java
@@ -0,0 +1,28 @@
+Picked up JAVA_TOOL_OPTIONS: -javaagent:H:\Code\Wakfuassistant\agent\wakfu-spy-agent.jar --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED
+/*
+ * Decompiled with CFR 0.152.
+ */
+public class coS {
+    private final byte mdw;
+    private final long mdx;
+    private final byte[] mdy;
+
+    coS(byte by, long l, byte[] byArray) {
+        this.mdw = by;
+        this.mdx = l;
+        this.mdy = byArray;
+    }
+
+    public byte aFW() {
+        return this.mdw;
+    }
+
+    public long Sn() {
+        return this.mdx;
+    }
+
+    public byte[] aKU() {
+        return this.mdy;
+    }
+}
+
